56a5ad8bae0964eafff62d7ec8c102af9e4817c0
sarhasan2 <pruebass3004@gmail.com>
2022-09-24 23:58:43 -0500

    probando html y css

---
+++ proyectocrud/src/main/java/com/proyectocrud/proyectocrud/controller/WebController.java
@@ -0,0 +1,15 @@
+package com.proyectocrud.proyectocrud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class WebController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+
+}
